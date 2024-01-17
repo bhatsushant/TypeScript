d0d8802a124e1661d50615dceb0560124439e1c0
Sushant <sush1393@gmail.com>
2024-01-17 00:03:05 -0500

    type inferences

---
+++ lesson-03/src/main.ts
@@ -0,0 +1,3 @@
+let num1: number = 4; //'inferences' the type of num1 as number even without explicitly mentioning a: number when a = 4
+
+const num2 = 2; //'inferences' the type of num2 as 4 as the type or value cannot be changes to anything once a variable is declared as const
