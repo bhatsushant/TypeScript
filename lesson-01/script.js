95f127fdde0065f635571dccf57feeaab6de0495
Sushant <sush1393@gmail.com>
2024-01-16 23:35:12 -0500

    setup typescript and learn usage of --noEmitOnError

---
+++ lesson-01/script.js
@@ -0,0 +1,5 @@
+var num1 = 5;
+var num2 = 3;
+var sum = function (num1, num2) { return num1 + num2; };
+console.log(sum);
+sum(num1, num2);
---
+++ lesson-01/script.ts
@@ -0,0 +1,8 @@
+const num1 = 5;
+const num2 = 3;
+
+const sum = (num1: number, num2: number) => num1 + num2;
+
+console.log(sum);
+
+sum(num1, num2);
